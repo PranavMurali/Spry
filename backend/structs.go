e285bb09e0a3a331b3f2096ee4a6e9d18b57d150
VoidlessVoid7 <voidlessvoid7@gmail.com>
2022-02-22 17:16:44 +0530

    reverse route check

---
+++ backend/structs.go
@@ -0,0 +1,58 @@
+package main
+
+// defines a standard geographical area
+type Place struct {
+	Name string
+	Lat  float64
+	Lng  float64
+}
+
+// defines the places.json input format (for reading only)
+type PlacesList struct {
+	Places []Place
+}
+
+// defines a standard bangalore bus route
+type Route struct {
+	RouteName   string
+	Origin      string
+	Destination string
+	Places      []string
+	RevPlaces   []string
+}
+
+type ShortestRoute struct {
+	RawRoute    Route
+	ForwardFlag bool
+}
+
+// defines the routes.json input format (for reading only)
+type RouteList struct {
+	Routes []Route
+}
+
+type Distance struct {
+	Destination_addresses []string
+	Origin_addresses      []string
+	Rows                  []Row
+}
+
+type Row struct {
+	Elements []Element `json:"elements"`
+}
+
+type Element struct {
+	Distance Distance_struct `json:"distance"`
+	Duration Duration_struct `json:"duration"`
+	Status   string          `json:"status"`
+}
+
+type Distance_struct struct {
+	Text  string `json:"text"`
+	Value int    `json:"value"`
+}
+
+type Duration_struct struct {
+	Text  string `json:"text"`
+	Value int    `json:"value"`
+}
